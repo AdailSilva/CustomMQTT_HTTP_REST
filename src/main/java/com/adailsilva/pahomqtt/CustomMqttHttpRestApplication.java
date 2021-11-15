aae3c082b61167c4f1d95ae44e08da6e85df60a8
Adail Silva <adail@bdmg.mg.gov.br>
2021-11-15 09:18:43 -0300

    Changes to packages structure

---
+++ src/main/java/com/adailsilva/pahomqtt/CustomMqttHttpRestApplication.java
@@ -0,0 +1,13 @@
+package com.adailsilva.pahomqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomMqttHttpRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomMqttHttpRestApplication.class, args);
+	}
+
+}
